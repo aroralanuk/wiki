aef6325545990fcd35c61b7ccfa468b336e1618b
Kunal Arora <kuarora@ucsd.edu>
2020-09-17 21:37:42 -0700

    entry pages done

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
 from . import util
-
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def displayEntry(request, title):
+    md = util.get_entry(title)
+    if not md:
+        return None
+    else:
+        return render(request,"encyclopedia/entry.html",{
+            "title":title,
+            "body": markdown2.markdown(md)
+        })
 
